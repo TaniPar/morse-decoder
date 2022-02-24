7d9b56a27b0cbbad95bbef1cf952646c8b6dc701
TaniPar <taniparfionova@gmail.com>
2022-02-24 14:10:56 +0300

    init: done this task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,39 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let result = '';
+    let arrKey = [];
+    let arrVal = [];
+    for(let key in MORSE_TABLE){
+        arrKey.push(key);
+        arrVal.push(MORSE_TABLE[key]);
+    }
+    for(let i = 0; i < expr.length; i+= 10){
+        let letter = '';
+        for (f=i; f< i+ 10; f++){
+            letter = `${letter}${expr[f]}`;
+        }
+        if (letter === "**********"){
+            result = `${result} `;
+        } else {
+            let b = '';
+           for(let j = 9; j > 0; j-=2){
+            let a =`${letter[j-1]}${letter[j]}`;
+            if(a === "10") {
+                b =`.${b}`; 
+            } else if(a === "11") {
+                b =`-${b}`;
+            }  
+        }
+        for(let j = 0; j < arrVal.length; j++){
+            if(b === arrKey[j]){
+                result = `${result}${arrVal[j]}`;
+            }
+        }
+        }
+    }
+    return result;
 }
 
 module.exports = {
